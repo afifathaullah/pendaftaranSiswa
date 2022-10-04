c3a68f231aa1292d130b2062adee9972a6da3931
Afif <afifwoles04@gmail.com>
2022-10-04 20:29:29 +0700

    Create pendaftaran_siswa (1).sql
    update

---
+++ pendaftaran_siswa/pendaftaran_siswa (1).sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2022 at 02:11 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jurusan`
+--
+-- Error reading structure for table pendaftaran_siswa.tb_jurusan: #1932 - Table 'pendaftaran_siswa.tb_jurusan' doesn't exist in engine
+-- Error reading data for table pendaftaran_siswa.tb_jurusan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pendaftaran_siswa`.`tb_jurusan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_siswa`
+--
+-- Error reading structure for table pendaftaran_siswa.tb_siswa: #1932 - Table 'pendaftaran_siswa.tb_siswa' doesn't exist in engine
+-- Error reading data for table pendaftaran_siswa.tb_siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pendaftaran_siswa`.`tb_siswa`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table pendaftaran_siswa.user: #1932 - Table 'pendaftaran_siswa.user' doesn't exist in engine
+-- Error reading data for table pendaftaran_siswa.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pendaftaran_siswa`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
